08fd721989cb2b5dbaf2e7a6e717f7643f55e522
Renertt <maksimvlasov415@gmail.com>
2025-09-24 21:56:27 +0800

    fileSystemRefactor

--- run.py
+++ run.py
@@ -3,32 +3,32 @@ import json
 import os
 
 def run_benchmark():
-    resultsDir = 'collectedData'
-    outputDir = 'executedNotebooks'
+    resultsDir = 'modelsInfo/collectedData'
+    outputDir = 'modelsInfo/executedNotebooks'
 
     os.makedirs(resultsDir, exist_ok=True)
     os.makedirs(outputDir, exist_ok=True)
 
     notebooks_to_run = {
-        'RandomForest': 'models/housePriceAnaliseRF.ipynb',
-        'XGBoost': 'models/housePriceAnaliseXGB.ipynb',
-        'LGBoost': 'models/housePriceAnaliseLGB.ipynb'
+        'RandomForest': 'modelsInfo/models/housePriceAnaliseRF.ipynb',
+        'XGBoost': 'modelsInfo/models/housePriceAnaliseXGB.ipynb',
+        'LGBoost': 'modelsInfo/models/housePriceAnaliseLGB.ipynb'
     }
     
     for modelName, notebook_path in notebooks_to_run.items():
         print(f"Starting {modelName}")
         pm.execute_notebook(
             notebook_path,
-            f'executedNotebooks/output_{os.path.basename(notebook_path)}',
+            f'modelsInfo/executedNotebooks/output_{os.path.basename(notebook_path)}',
             cwd='.'
         )
         print(f"{modelName} finished")
 
     finalResults = []
     resultFiles = [
-        'collectedData/resultsRF.json',
-        'collectedData/resultsXGB.json',
-        'collectedData/resultsLGB.json'
+        'modelsInfo/collectedData/resultsRF.json',
+        'modelsInfo/collectedData/resultsXGB.json',
+        'modelsInfo/collectedData/resultsLGB.json'
                    ]
     
     for file_path in resultFiles:
